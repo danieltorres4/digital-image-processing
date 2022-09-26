5b8e236653c335719ef7ee47eff1dfd9953af862
danieltorres4 <dst990502@outlook.com>
2022-09-26 16:39:46 -0500

    Changing image sizes

--- Practice3/P3E2.m
+++ Practice3/P3E2.m
@@ -1,8 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%       Practice 3 - Equalization         %%% 
+%%%             Team members                %%%
+%%%     +Herrera Godina Adriana Jocelyn     %%%
+%%%        +Miranda Miranda Emiliano        %%%
+%%%      +Nicolás Marín Brian Geovanny      %%%
+%%%      +Sanchez Torres Sergio Daniel      %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %EJERCICIO 2
-%a) Desplegar las imágenes satelitales cortando o eliminando las zonas donde la información es nula. Desplegar
+%a) Desplegar las imágenes satelitales cortando o 
+% eliminando las zonas donde la información es nula. Desplegar
 % además el histograma correspondiente a cada una de ellas.
-%zonaA = imread("C:\Users\Adri\Documents\Adriana\2023-1\PROCESAMIENTO DIGITAL DE IMAGENES\zonaA_b5.tif");
-zonaB=imread("C:\Users\Adri\Documents\Adriana\2023-1\PROCESAMIENTO DIGITAL DE IMAGENES\zonaB_b5.tif");
+zonaA = imread("zonaA_b5.tif");
+zonaB = imread("zonaB_b5.tif");
 %J=imcrop(zonaA);%selecciona con el mouse la region a recortar
 %L=imcrop(zonaB);
 %figure(2)
@@ -10,11 +19,17 @@ zonaB=imread("C:\Users\Adri\Documents\Adriana\2023-1\PROCESAMIENTO DIGITAL DE IM
 %imshow(L)
 %imshow(J) % muestra la imagen recortada
 imagensinzonanula = imcrop(zonaA,[0.5 0.5 1765 1500]);
-figure(4)
+sz1 = size(imagensinzonanula);
+figure(1)
 imshow(imagensinzonanula)
+
 imagensinzonanulaB = imcrop(zonaB,[1222.5 4.5 1278 1496]);
-figure(5)
+sz2 = size(imagensinzonanulaB);
+imagensinzonanulaB = imresize(imagensinzonanulaB, [1500,1766]);
+sz3 = size(imagensinzonanulaB);
+figure(2)
 imshow(imagensinzonanulaB)
+
 %GENERANDO HISTOGRAMAS
 histograma2 = histeq(imagensinzonanula);
 figure("name", "Imagen ZonaA y su histograma")
@@ -22,7 +37,7 @@ subplot(1,2,1)
 imhist(imagensinzonanula)
 subplot(1,2,2)
 imshow(histograma2)
-title('HISTOGRAMA DE ZONA A')
+title('ZONA A')
 
 histograma3 = histeq(imagensinzonanulaB);
 figure("name", "Imagen ZonaB y su histograma")
