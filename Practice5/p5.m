f12223539de0e22551ee264f4563999a2d3d377e
danieltorres4 <dst990502@outlook.com>
2022-10-29 10:05:50 -0500

    Creating Practice 5 folder

---
+++ Practice5/p5.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%       Practice 5 - Linear, Circular,    %%%
+%%%           and Fourier Convolution       %%%
+%%%             Team members                %%%
+%%%     +Herrera Godina Adriana Jocelyn     %%%
+%%%        +Miranda Miranda Emiliano        %%%
+%%%      +Nicolás Marín Brian Geovanny      %%%
+%%%      +Sanchez Torres Sergio Daniel      %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
