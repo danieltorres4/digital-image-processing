7f92f6d754baf9b1c6dc0208eedba91cea412659
danieltorres4 <dst990502@outlook.com>
2022-10-29 14:50:32 -0500

    Creating the source file and uploading the pentagon image

--- Practice5/p5.m
+++ Practice5/p5.m
@@ -7,3 +7,14 @@
 %%%      +Nicolás Marín Brian Geovanny      %%%
 %%%      +Sanchez Torres Sergio Daniel      %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all;
+
+originalImage = imread('pentagon256x256.tif');
+
+figure("name","Original Image")
+imshow(originalImage)
+
+%linearConv7x7 = conv2(originalImage,'full');
+%linearConv9x9
+%linearConv11x11
