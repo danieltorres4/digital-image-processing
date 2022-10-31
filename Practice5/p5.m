0fe676ca732bd1f85578ccce272137a3b49d3abb
Adriana Herrera Godina <91992994+adrianaSung@users.noreply.github.com>
2022-10-30 18:50:21 -0600

    Actualizando P5

--- Practice5/p5.m
+++ Practice5/p5.m
@@ -49,7 +49,17 @@ title("CONV2 11x11 USING FULL");
 
 %2- Obtener la DFT de la imagen original y desplegarla de manera amplificada 
 %utilizando el logaritmo del módulo de la DFT para dicha amplificación. 
+pentImg = imread('pentagon256x256.tif');
+imshow(pentImg,'InitialMagnification','fit')
+F = fft2(pentImg);
+F2 = log(abs(F));
+imshow(F2,[-1 5],'InitialMagnification','fit');
+colormap(jet); 
 %Cambiar el eje de coordenadas (comando MATLAB ffshift) y nuevamente amplificar.
+Y=fft2(pentImg);
+F3 = log(abs(Y));
+imagesc(abs(fftshift(F3)));
+
 
 
 %3- Obtener la convolución circular ($\otimes$) de la imagen con el filtro 
