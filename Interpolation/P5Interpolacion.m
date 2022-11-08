5f7c57966bde7b3be4d275563141c6816d98947b
Adriana Herrera Godina <91992994+adrianaSung@users.noreply.github.com>
2022-11-07 19:45:59 -0600

    Update P5

--- Interpolation/P5Interpolacion.m
+++ Interpolation/P5Interpolacion.m
@@ -1,3 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%        Practice - Interpolation         %%%
+%%%             Team members                %%%
+%%%     +Herrera Godina Adriana Jocelyn     %%%
+%%%        +Miranda Miranda Emiliano        %%%
+%%%      +Nicolás Marín Brian Geovanny      %%%
+%%%      +Sanchez Torres Sergio Daniel      %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Ejercicio 1 - Sobremuestreo espacial
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Ejercicio 1.1
 A = imread('pentagon.tif');
 
@@ -42,27 +54,35 @@ for i=1:length(A)
     filas = [c_col2(i,1:end);ceros_row4];
     c_pad2 = [c_pad2;filas];
 end
+%1.2 Obtenga a la magnitud del espectro de la DFT (abs) de cada una de las 
+%imágenes sobremuestreadas y de la imagen original. Despliegue los 
+%resultados en una misma figura para efectos de comparación (en Matlab se 
+%puede usar el comando subplot). Recuerde usar fftshift para centrar los 
+%espectros y una función de escalamiento para el despliegue, ejemplo: 
+%ImFDespliegue=log(1.0 + ImF), donde ImF es la DFT de la imagen con sobremuestreo.
+dftor = fftshift(abs(log(fft2(A))));
+dft2x2 = fftshift(abs(log(fft2(c_pad))));
+dft4x4 = fftshift(abs(log(fft2(c_pad2))));
 
-figure("name","sobremuestreo 2x2");
-imshow(c_pad);
-%zoom(10)
-title("sobremuestreo de 2x2")
+figure('name',"1.2");
+subplot(2,2,1);
+imshow(dftor, []);
+title("DFT DE IMAGEN ORIGINAL");
 
-figure("name","sobremuestreo 4x4");
-imshow(c_pad2);
-%zoom(10)
-title("sobremuestreo de 4x4")
-%Ejercicio1.2
+subplot(2,2,2);
+imshow(dft2x2, []);
+title("DFT DE IMAGEN CON SOBREMUESTREO 2x2");
 
-figure("name"," DFT sobremuestreo 2x2");
-imshow(fftshift(abs(log(fft2(c_pad)))), []);
-title("DFT sobremuestreo 2x2");
+subplot(2,2,3);
+imshow(dft4x4, []);
+title("DFT DE IMAGEN CON SOBREMUESTREO 4x4");
 
-figure("name"," DFT sobremuestreo 4x4");
-imshow(fftshift(abs(log(fft2(c_pad2)))), []);
-title("DFT sobremuestreo 2x2");
 
-%Ejercicio 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Ejercicio 2 - Interpolación espacial. Interpole las imágenes con
+%sobremuestreo obtenidas en el inciso anterior (con factores $T \uparrow = 
+%2 \times 2$ y $T \uparrow = 4 \times 4$) usando interpoladores:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %convolución 2x2
 aux = 0;
 for i=1:length(c_pad)
@@ -112,7 +132,7 @@ conv(length(conv),:) = []; %se elimina la ultima fila
 conv(:,length(conv)) = []; %se elimina la ultima columna
 
 
-figure("name","Interpolador de orden cero 2x2");
+figure("name","Interpolador de orden cero 2x2");  %AGREGAR EN SUBPLOT
 imshow(c_pad);
 %zoom(10)
 title("Interpolador de orden cero de 2x2")
@@ -153,8 +173,8 @@ figure("name", "Interpolacion cubica 4x4");
 imshow(intercubica4x4)
 
 %Ejercicio 2.2
-figure("name","Zoom 2x2");
-imshow(fftshift(abs(log(fft2(c_pad)))), []);
+subplot(3, 2, 1)
+imshow(fftshift(abs(log(fft2(c_pad)))), []);  
 title("Zoom 4x4")
 subplot(3, 2, 2)
 imshow(fftshift(abs(log(fft2(c_pad2)))), []);
@@ -174,6 +194,62 @@ subplot(3, 2, 6)
 imshow(fftshift(abs(log(fft2(intercubica4x4)))), []);
 title("DFT Interpolación Cubica 4x4")
 
+%%%%%%%%%%%%
+%Ejercicio 3
+%%%%%%%%%%%%
+%3.1 DFT de imagen original
+DFTOriginal = fft2(A);
+imshow(log(DFTOriginal), []);
+title("3.1 DFT ORIGINAL")
+%3.2 DFT con ceros alrededor
+DFTwithPading2x2 = padarray(fftshift(DFTOriginal), fix(size(A)/2), 0, 'both');
+DFTwithPading4x4 = padarray(fftshift(DFTOriginal), fix(size(A)*(3/2)), 0, 'both'); %AGREGAR SUBPLOT
+figure("name", "DFT(abs) con ceros alrededor 2x2")
+imshow((log(DFTwithPading2x2)), [])
+title("3.2 DFT CON CEROS ALREDEDOR 2x2")
+figure("name", "DFT(abs) con ceros alrededor 4x4")
+imshow((log(DFTwithPading4x4)), [])
+title("3.2 DFT CON CEROS ALREDEDOR 4x4")
+
+%3.3 Comparacion de DFT
+subplot(4, 1, 1)
+imshow(fftshift(abs(log(fft2(DFTwithPading2x2)))), []);
+title("DFT Interpolación orden cero 2x2")
+subplot(4, 1, 2)
+imshow(fftshift(abs(log(fft2(interLineal2x2)))), []);
+title("DFT Interpolación Lineal 2x2")
+subplot(4, 1, 3)
+imshow(fftshift(abs(log(fft2(intercubica2x2)))), []);
+title("DFT Interpolación cubica 2x2")
+subplot(4, 1, 4)
+imshow((log(DFTwithPading2x2)), [])
+title("DFT Interpolación en frecuencia 2x2")
+
+figure();
+subplot(4, 1, 1)
+imshow(fftshift(abs(log(fft2(c_pad2)))), []);
+title("DFT Interpolación orden cero 4x4")
+subplot(4, 1, 2)
+imshow(fftshift(abs(log(fft2(interLineal4x4)))), []);
+title("DFT Interpolación Lineal 4x4")
+subplot(4, 1, 3)
+imshow(fftshift(abs(log(fft2(intercubica4x4)))), []);
+title("DFT Interpolación cubica 4x4")
+subplot(4, 1, 4)
+imshow((log(DFTwithPading4x4)), [])
+title("DFT Interpolación en frecuencia 4x4")
+
+%3.5 DFT Inversa
+DFTinverse2x2 = abs(ifft2(DFTwithPading2x2));
+DFTinverse4x4 = abs(ifft2(DFTwithPading4x4));
+figure("name", "DFT Inversa 2x2")
+imshow(DFTinverse2x2, [])
+title("3.5 DFT INVERSA 1")
+figure("name", "DFT Inversa 4x4")
+imshow(DFTinverse4x4, [])
+title("3.5 DFT INVERSA 2")
+
+
 function B = UPSAMPLE(A,N,M)
     B = zeros([size(A,1)*M size(A,2)*N]);
     B(1:M:end,1:N:end) = A;
@@ -190,4 +266,5 @@ function f = cubic(x)
                         ((1 < absx) & (absx <= 2));
                     
 end
+
 
